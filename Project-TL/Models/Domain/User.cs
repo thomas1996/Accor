33066125c406b115f65dfecfd71090275573e893
thomas1996 <thomas.lefevere@hotmail.com>
2017-02-27 15:11:55 +0100

    Add project files.

---
+++ Project-TL/Models/Domain/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_TL.Models.Domain
+{
+    public class User
+    {
+       public int Id  {get;set;}
+       public string Username { get; set; }
+       public string Password { get; set; }
+        public string Location { get; set; }
+        public bool Admin { get; set; }
+        public string ad;
+    
+
+
+        
+    }
+}
---
+++ Project-TL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_TL.Startup))]
+namespace Project_TL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
